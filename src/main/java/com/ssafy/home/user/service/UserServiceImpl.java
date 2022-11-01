b60c7920a52e07dc82730b6bceeaea975b698984
kakaopanda <kakao_panda@naver.com>
2022-11-01 14:47:19 +0900

    delete function

--- src/main/java/com/ssafy/home/user/service/UserServiceImpl.java
+++ src/main/java/com/ssafy/home/user/service/UserServiceImpl.java
@@ -38,4 +38,9 @@ public class UserServiceImpl implements UserService {
 		userMapper.updateUser(user);
 	}
 
+	@Override
+	public void deleteUser(String userId) throws Exception {
+		userMapper.deleteUser(userId);
+	}
+
 }
