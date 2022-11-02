91f3e1e39c6268ee0e467c17e04ecf784f7d00b1
JSP <pjs12041204@gmail.com>
2022-11-02 09:54:55 +0900

    swagger 적용

--- src/main/java/com/ssafy/home/user/controller/UserController.java
+++ src/main/java/com/ssafy/home/user/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -25,8 +26,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.home.user.entity.User;
 import com.ssafy.home.user.service.UserService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/users")
+@CrossOrigin("*")
+@Api("유저 컨트롤러 API")
 public class UserController {
 
 	private final UserService userService;
@@ -34,18 +42,6 @@ public class UserController {
 	@Autowired
 	public UserController(UserService userService) {
 		this.userService = userService;
-	}
-
-	@GetMapping("/user/{userId}")
-	public ResponseEntity<?> getUserInfo(@PathVariable("userId") String userId) {
-		
-		try {
-			User user = userService.getUserInfo(userId);
-			return new ResponseEntity<User> (user, HttpStatus.OK);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new ResponseEntity<String>("getUserDetail Fail", HttpStatus.NOT_ACCEPTABLE);
-		}
 	}
 
 	@PostMapping("/login")
@@ -84,7 +80,26 @@ public class UserController {
 		session.invalidate();
 		return new ResponseEntity<String>("logout Ok", HttpStatus.OK);
 	}
-
+	
+	@ApiOperation(value = "회원정보", notes = "회원한명에 대한 정보.")
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "userid", value = "아이디", required = true, dataType = "String", paramType = "path")
+//			@ApiImplicitParam(name = "param1", value = "파라미터1", required = true, dataType = "String", paramType = "query"),
+//			@ApiImplicitParam(name = "param2", value = "파마미터2", required = false, dataType = "int", paramType = "query") 
+	})
+	@GetMapping("/user/{userId}")
+	public ResponseEntity<?> getUserInfo(@PathVariable("userId") String userId) {
+		
+		try {
+			User user = userService.getUserInfo(userId);
+			return new ResponseEntity<User> (user, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>("getUserDetail Fail", HttpStatus.NOT_ACCEPTABLE);
+		}
+	}
+	
+	@ApiOperation(value = "회원등록", notes = "회원의 정보를 받아 처리.")
 	@PostMapping("/join")
 	public ResponseEntity<?> join(@RequestBody User user) {
 		System.out.println("/join-create");
@@ -98,6 +113,7 @@ public class UserController {
 		}
 	}
 
+	@ApiOperation(value = "회원정보수정", notes = "회원정보를 수정합니다.")
 	@PutMapping("/join")
 	public ResponseEntity<?> update(@RequestBody User user) {
 		System.out.println("/join-update");
@@ -111,6 +127,7 @@ public class UserController {
 		}
 	}
 	
+	@ApiOperation(value = "회원정보삭제", notes = "회원정보를 삭제합니다.")
 	@DeleteMapping("/remove/{userId}")
 	public ResponseEntity<?> remove(@PathVariable("userId") String userId) {
 		
