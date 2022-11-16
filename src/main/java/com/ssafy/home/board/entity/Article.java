dcb887b60121a56a18448208baf160d809bdeb8b
JSP <pjs12041204@gmail.com>
2022-11-16 10:05:44 +0900

    update insert article function

--- src/main/java/com/ssafy/home/board/entity/Article.java
+++ src/main/java/com/ssafy/home/board/entity/Article.java
@@ -6,10 +6,12 @@ import java.sql.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Article {
 
 	private int articleNo;
@@ -18,6 +20,7 @@ public class Article {
 	private String content;
 	private int hit;
 	private Timestamp registerTime;
-	private int articleTypeId;
-	private String articleTypeName;
+	private int articleProp;
+//	private int articleTypeId;
+//	private String articleTypeName;
 }
